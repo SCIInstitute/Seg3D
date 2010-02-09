db8773e08cf78b724a72114b6f26cf13bd4d9807
Jianrong Shu <jshu@sci.utah.edu>
2010-02-09 17:20:19 +0000

    Fixed an error for gcc

--- src/Utils/Geometry/Matrix.cc
+++ src/Utils/Geometry/Matrix.cc
@@ -1,29 +1,29 @@
 /*
-   For more information, please see: http://software.sci.utah.edu
+For more information, please see: http://software.sci.utah.edu
 
-   The MIT License
+The MIT License
 
-   Copyright (c) 2009 Scientific Computing and Imaging Institute,
-   University of Utah.
+Copyright (c) 2009 Scientific Computing and Imaging Institute,
+University of Utah.
 
-   
-   Permission is hereby granted, free of charge, to any person obtaining a
-   copy of this software and associated documentation files (the "Software"),
-   to deal in the Software without restriction, including without limitation
-   the rights to use, copy, modify, merge, publish, distribute, sublicense,
-   and/or sell copies of the Software, and to permit persons to whom the
-   Software is furnished to do so, subject to the following conditions:
 
-   The above copyright notice and this permission notice shall be included
-   in all copies or substantial portions of the Software.
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
 
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-   DEALINGS IN THE SOFTWARE.
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
 */
 
 #include <boost/numeric/ublas/vector.hpp>
@@ -37,27 +37,25 @@ namespace Utils {
 
 namespace ublas = boost::numeric::ublas;
 
-const Matrix Matrix::IDENTITY_C = boost::numeric::ublas::identity_matrix<double>(4);
-const Matrix Matrix::ZERO_C = boost::numeric::ublas::zero_matrix<double>(4,4);
+const Matrix Matrix::IDENTITY_C(boost::numeric::ublas::identity_matrix<double>(4));
+const Matrix Matrix::ZERO_C(boost::numeric::ublas::zero_matrix<double>(4,4));
 const double Matrix::EPSILON_C = 1e-7;
 
-Vector 
-Matrix::operator *(const Vector &rhs) const
+Vector Matrix::operator *(const Vector &rhs) const
 {
   ublas::vector<double> v(4);
   v[0] = rhs[0];
   v[1] = rhs[1];
   v[2] = rhs[2];
   v[3] = 1.0;
-  
+
   ublas::vector<double> result(4);
   result = ublas::prec_prod(*this, v);
-  
+
   return Vector(result[0]/result[3], result[1]/result[3], result[2]/result[3]);
 }
 
-VectorF 
-Matrix::operator *(const VectorF &rhs) const
+VectorF Matrix::operator *(const VectorF &rhs) const
 {  
   ublas::vector<double> v(4);
   v[0] = rhs[0];
@@ -69,12 +67,11 @@ Matrix::operator *(const VectorF &rhs) const
   result = ublas::prod(*this, v);
 
   return VectorF(static_cast<float>(result[0]/result[3]), 
-                          static_cast<float>(result[1]/result[3]), 
-                          static_cast<float>(result[2]/result[3]));
+              static_cast<float>(result[1]/result[3]), 
+              static_cast<float>(result[2]/result[3]));
 }
 
-Point 
-Matrix::operator*(const Point& rhs) const
+Point Matrix::operator*(const Point& rhs) const
 {
   ublas::vector<double> v(4);
   v[0] = rhs[0];
@@ -84,12 +81,11 @@ Matrix::operator*(const Point& rhs) const
 
   ublas::vector<double> result(4);
   result = ublas::prec_prod(*this, v);
-  
+
   return Point(result[0]/result[3], result[1]/result[3], result[2]/result[3]);
 }
 
-PointF 
-Matrix::operator*(const PointF& rhs) const
+PointF Matrix::operator*(const PointF& rhs) const
 {
   ublas::vector<double> v(4);
   v[0] = rhs[0];
@@ -101,8 +97,8 @@ Matrix::operator*(const PointF& rhs) const
   result = ublas::prod(*this, v);
 
   return PointF(static_cast<float>(result[0]/result[3]), 
-                        static_cast<float>(result[1]/result[3]), 
-                        static_cast<float>(result[2]/result[3]));
+              static_cast<float>(result[1]/result[3]), 
+              static_cast<float>(result[2]/result[3]));
 }
 
 bool Invert(const Matrix& m, Matrix& inverse)
@@ -110,13 +106,13 @@ bool Invert(const Matrix& m, Matrix& inverse)
   Matrix A(m);
   ublas::permutation_matrix<std::size_t> pm(A.size1());
   inverse = Matrix::IDENTITY_C;
-  
+
   Matrix::size_type singular = ublas::lu_factorize(A, pm);
   if (singular != 0)
   {
     return false;
   }
-  
+
   // If the determinant of A is less than a small value, 
   // consider it singular.
   double det = A(0, 0) * A(1, 1) * A(2, 2) * A(3, 3);
@@ -124,9 +120,9 @@ bool Invert(const Matrix& m, Matrix& inverse)
   {
     return false;
   }
-  
+
   ublas::lu_substitute(A, pm, inverse);
-  
+
   return true;
 }
 
--- src/Utils/Geometry/Matrix.h
+++ src/Utils/Geometry/Matrix.h
@@ -1,29 +1,29 @@
 /*
-   For more information, please see: http://software.sci.utah.edu
+For more information, please see: http://software.sci.utah.edu
 
-   The MIT License
+The MIT License
 
-   Copyright (c) 2009 Scientific Computing and Imaging Institute,
-   University of Utah.
+Copyright (c) 2009 Scientific Computing and Imaging Institute,
+University of Utah.
 
-   
-   Permission is hereby granted, free of charge, to any person obtaining a
-   copy of this software and associated documentation files (the "Software"),
-   to deal in the Software without restriction, including without limitation
-   the rights to use, copy, modify, merge, publish, distribute, sublicense,
-   and/or sell copies of the Software, and to permit persons to whom the
-   Software is furnished to do so, subject to the following conditions:
 
-   The above copyright notice and this permission notice shall be included
-   in all copies or substantial portions of the Software.
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
 
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-   DEALINGS IN THE SOFTWARE.
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
 */
 
 #ifndef UTILS_GEOMETRY_MATRIX_H
@@ -39,30 +39,30 @@ namespace Utils {
 
 class Matrix : 
   public boost::numeric::ublas::matrix<double, boost::numeric::ublas::column_major> {
-  
+
   typedef boost::numeric::ublas::matrix<double, boost::numeric::ublas::column_major> base_type;
-    
-  public:
-    inline Matrix() : base_type(4, 4) {}
+
+public:
+  inline Matrix() : base_type(4, 4) {}
 
   inline Matrix(const double m[4][4]) : base_type(4, 4)
   {
     this->data(m);
   }
-    
-    Matrix(const base_type& m) : base_type(m) 
-    {
-      assert(m.size1() == 4 && m.size2() == 4);
-    }
-    
-    ~Matrix() {}
-    
-    inline Matrix& operator=(const base_type& m)
-    {
-      assert(m.size1() == 4 && m.size2() == 4);
-      assign(m);
-      return (*this);
-    }
+
+  Matrix(const base_type& m) : base_type(m) 
+  {
+    assert(m.size1() == 4 && m.size2() == 4);
+  }
+
+  ~Matrix() {}
+
+  inline Matrix& operator=(const base_type& m)
+  {
+    assert(m.size1() == 4 && m.size2() == 4);
+    assign(m);
+    return (*this);
+  }
 
   inline double* data()
   {
@@ -75,24 +75,24 @@ class Matrix :
   }
 
   inline void data(const double m[4][4]);
-    
-    Vector operator*(const Vector& rhs) const;
-    VectorF operator*(const VectorF& rhs) const;
-    Point operator*(const Point& rhs) const;
-    PointF operator*(const PointF& rhs) const;
-    
-    
-  private:
-    
-  public:
-    // Identity matrix
-    const static Matrix IDENTITY_C;
-    
-    // Zero matrix
-    const static Matrix ZERO_C;
-    
-    // Threshold value of determinant for classifying a matrix as singular
-    const static double EPSILON_C;
+
+  Vector operator*(const Vector& rhs) const;
+  VectorF operator*(const VectorF& rhs) const;
+  Point operator*(const Point& rhs) const;
+  PointF operator*(const PointF& rhs) const;
+
+
+private:
+
+public:
+  // Identity matrix
+  const static Matrix IDENTITY_C;
+
+  // Zero matrix
+  const static Matrix ZERO_C;
+
+  // Threshold value of determinant for classifying a matrix as singular
+  const static double EPSILON_C;
 };
 
 inline void Matrix::data(const double m[4][4])
