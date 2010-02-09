9673fb9fbca3c2b5a64b03f7d9944e3f42ed4677
Jianrong Shu <jshu@sci.utah.edu>
2010-02-09 01:33:58 +0000

    Added a column major Matrix class, a subclass of boost uBLAS matrix.

--- src/Utils/Geometry/Matrix.cc
+++ src/Utils/Geometry/Matrix.cc
@@ -26,8 +26,108 @@
    DEALINGS IN THE SOFTWARE.
 */
 
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/matrix_expression.hpp>
+#include <boost/numeric/ublas/lu.hpp>
+
 #include <Utils/Geometry/Matrix.h>
+#include <Utils/Math/MathFunctions.h>
 
 namespace Utils {
 
+namespace ublas = boost::numeric::ublas;
+
+const Matrix Matrix::IDENTITY_C = boost::numeric::ublas::identity_matrix<double>(4);
+const Matrix Matrix::ZERO_C = boost::numeric::ublas::zero_matrix<double>(4,4);
+const double Matrix::EPSILON_C = 1e-7;
+
+Vector 
+Matrix::operator *(const Vector &rhs) const
+{
+  ublas::vector<double> v(4);
+  v[0] = rhs[0];
+  v[1] = rhs[1];
+  v[2] = rhs[2];
+  v[3] = 1.0;
+  
+  ublas::vector<double> result(4);
+  result = ublas::prec_prod(*this, v);
+  
+  return Vector(result[0]/result[3], result[1]/result[3], result[2]/result[3]);
 }
+
+VectorF 
+Matrix::operator *(const VectorF &rhs) const
+{  
+  ublas::vector<double> v(4);
+  v[0] = rhs[0];
+  v[1] = rhs[1];
+  v[2] = rhs[2];
+  v[3] = 1.0;
+
+  ublas::vector<double> result(4);
+  result = ublas::prod(*this, v);
+
+  return VectorF(static_cast<float>(result[0]/result[3]), 
+                          static_cast<float>(result[1]/result[3]), 
+                          static_cast<float>(result[2]/result[3]));
+}
+
+Point 
+Matrix::operator*(const Point& rhs) const
+{
+  ublas::vector<double> v(4);
+  v[0] = rhs[0];
+  v[1] = rhs[1];
+  v[2] = rhs[2];
+  v[3] = 1.0;
+
+  ublas::vector<double> result(4);
+  result = ublas::prec_prod(*this, v);
+  
+  return Point(result[0]/result[3], result[1]/result[3], result[2]/result[3]);
+}
+
+PointF 
+Matrix::operator*(const PointF& rhs) const
+{
+  ublas::vector<double> v(4);
+  v[0] = rhs[0];
+  v[1] = rhs[1];
+  v[2] = rhs[2];
+  v[3] = 1.0;
+
+  ublas::vector<double> result(4);
+  result = ublas::prod(*this, v);
+
+  return PointF(static_cast<float>(result[0]/result[3]), 
+                        static_cast<float>(result[1]/result[3]), 
+                        static_cast<float>(result[2]/result[3]));
+}
+
+bool Invert(const Matrix& m, Matrix& inverse)
+{
+  Matrix A(m);
+  ublas::permutation_matrix<std::size_t> pm(A.size1());
+  inverse = Matrix::IDENTITY_C;
+  
+  Matrix::size_type singular = ublas::lu_factorize(A, pm);
+  if (singular != 0)
+  {
+    return false;
+  }
+  
+  // If the determinant of A is less than a small value, 
+  // consider it singular.
+  double det = A(0, 0) * A(1, 1) * A(2, 2) * A(3, 3);
+  if (Abs(det) < Matrix::EPSILON_C)
+  {
+    return false;
+  }
+  
+  ublas::lu_substitute(A, pm, inverse);
+  
+  return true;
+}
+
+} // End namespace Utils
--- src/Utils/Geometry/Matrix.h
+++ src/Utils/Geometry/Matrix.h
@@ -29,9 +29,58 @@
 #ifndef UTILS_GEOMETRY_MATRIX_H
 #define UTILS_GEOMETRY_MATRIX_H
 
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/matrix_expression.hpp>
+
+#include <Utils/Geometry/Point.h>
+#include <Utils/Geometry/Vector.h>
+
 namespace Utils {
 
+class Matrix : 
+  public boost::numeric::ublas::matrix<double, boost::numeric::ublas::column_major> {
+  
+  typedef boost::numeric::ublas::matrix<double, boost::numeric::ublas::column_major> base_type;
+    
+  public:
+    Matrix() : base_type(4, 4) {}
+    
+    Matrix(const base_type& m) : base_type(m) 
+    {
+      assert(m.size1() == 4 && m.size2() == 4);
+    }
+    
+    ~Matrix() {}
+    
+    inline Matrix& operator=(const base_type& m)
+    {
+      assert(m.size1() == 4 && m.size2() == 4);
+      assign(m);
+      return (*this);
+    }
+    
+    Vector operator*(const Vector& rhs) const;
+    VectorF operator*(const VectorF& rhs) const;
+    Point operator*(const Point& rhs) const;
+    PointF operator*(const PointF& rhs) const;
+    
+    
+  private:
+    
+  public:
+    // Identity matrix
+    const static Matrix IDENTITY_C;
+    
+    // Zero matrix
+    const static Matrix ZERO_C;
+    
+    // Threshold value of determinant for classifying a matrix as singular
+    const static double EPSILON_C;
+};
 
+
+// Compute the inverse of the input matrix using LU decomposition
+bool Invert(const Matrix& m, Matrix& inverse);
 
 } // End namespace Utils
 
