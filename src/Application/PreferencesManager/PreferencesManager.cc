35275a26a03e2a3d821727db74b6e00552610271
Alan Morris <amorris@sci.utah.edu>
2012-02-01 00:18:04 +0000

    Fixed reversed logic in PreferencesManager.  This error caused new Seg3D installations to default users's projects to the executable location (e.g. C:\Program Files (x86)\Seg3D2 2.1 on windows) (usually unwritable), instead of C:\Users\<user>\Documents\Seg3D-Projects.
    None of the developers noticed likely because we all already have
    preferences.xml from previous versions.

--- src/Application/PreferencesManager/PreferencesManager.cc
+++ src/Application/PreferencesManager/PreferencesManager.cc
@@ -137,14 +137,14 @@ Core::Color PreferencesManager::get_color( int index ) const
 void PreferencesManager::initialize_states()
 {
   boost::filesystem::path user_path;
-  if (! Core::Application::Instance()->get_user_directory( user_path ) )
-    {
-        user_path = user_path / ( Core::Application::GetApplicationName() + "-Projects" );
+  if (Core::Application::Instance()->get_user_directory( user_path ) )
+  {
+    user_path = user_path / ( Core::Application::GetApplicationName() + "-Projects" );
   }
-    else
-    {
-        user_path = boost::filesystem::current_path();
-    }
+  else
+  {
+    user_path = boost::filesystem::current_path();
+  }
   boost::filesystem::path desktop_path;
   Core::Application::Instance()->get_user_desktop_directory( desktop_path );
 
