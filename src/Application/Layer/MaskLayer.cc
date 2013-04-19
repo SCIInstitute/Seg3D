6a5332f3adae57cfff99fbcf675794f2732113da
Ayla Khan <ayla@sci.utah.edu>
2013-04-19 01:20:36 +0000

    Hook up min and max display for mask layers (bug #810).

--- src/Application/Layer/MaskLayer.cc
+++ src/Application/Layer/MaskLayer.cc
@@ -113,6 +113,9 @@ void MaskLayerPrivate::initialize_states()
   // == Keep track of the calculated volume and put it in the UI
   this->layer_->add_state( "counted_pixels", this->layer_->counted_pixels_state_, "N/A" );
 
+  this->layer_->add_state( "min", this->layer_->min_value_state_, std::numeric_limits< double >::quiet_NaN() );
+  this->layer_->add_state( "max", this->layer_->max_value_state_, std::numeric_limits< double >::quiet_NaN() );
+
   this->update_mask_info();
 }
 
@@ -132,6 +135,9 @@ void MaskLayerPrivate::update_mask_info()
 {
   this->layer_->centering_state_->set( 
     this->layer_->get_grid_transform().get_originally_node_centered() ? "node" : "cell" );
+
+  this->layer_->min_value_state_->set( this->mask_volume_->get_min() );
+  this->layer_->max_value_state_->set( this->mask_volume_->get_max() );
 }
 
 MaskLayer::MaskLayer( const std::string& name, const Core::MaskVolumeHandle& volume ) :
