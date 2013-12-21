43b4d7ad2a37718652c8fc7de2fc5b7d28cecb5a
Ayla Khan <ayla@sci.utah.edu>
2013-12-21 01:07:44 +0000

    Migrate and save custom ITK transforms.

---
+++ src/Core/ITKCommon/itkInverseTransform.h
@@ -0,0 +1,141 @@
+/*
+ For more information, please see: http://software.sci.utah.edu
+ 
+ The MIT License
+ 
+ Copyright (c) 2013 Scientific Computing and Imaging Institute,
+ University of Utah.
+ 
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a
+ copy of this software and associated documentation files (the "Software"),
+ to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ and/or sell copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ DEALINGS IN THE SOFTWARE.
+*/
+
+
+// File         : itkInverseTransform.h
+// Author       : Pavel A. Koshevoy
+// Created      : 2005/06/03 10:16
+// Copyright    : (C) 2004-2008 University of Utah
+// Description  : A inverse transform class.
+
+#ifndef __itkInverseTransform_h
+#define __itkInverseTransform_h
+
+// system includes:
+#include <assert.h>
+
+// ITK includes:
+#include <itkTransform.h>
+#include <itkMacro.h>
+
+
+//----------------------------------------------------------------
+// itk::InverseTransform
+// 
+namespace itk
+{
+  //----------------------------------------------------------------
+  // InverseTransform
+  //
+  template <class ForwardTransform>
+  class InverseTransform :
+    public Transform< typename ForwardTransform::ScalarType,
+          ForwardTransform::OutputSpaceDimension,
+          ForwardTransform::InputSpaceDimension >
+  {
+  public:
+    /** Standard class typedefs. */
+    typedef InverseTransform Self;
+    
+    typedef Transform< typename ForwardTransform::ScalarType,
+           ForwardTransform::OutputSpaceDimension,
+           ForwardTransform::InputSpaceDimension > Superclass;
+    
+    typedef SmartPointer< Self >  Pointer;
+    typedef SmartPointer< const Self >  ConstPointer;
+    
+    /** Base inverse transform type. */
+    typedef typename Superclass::InverseTransformType InverseTransformType;
+    typedef SmartPointer< InverseTransformType > InverseTransformPointer;
+    
+    /** New method for creating an object using a factory. */
+    itkNewMacro(Self);
+    
+    /** Run-time type information (and related methods). */
+    itkTypeMacro( InverseTransform, Transform );
+    
+    /** Standard scalar type for this class. */
+    typedef typename Superclass::ScalarType ScalarType;
+    
+    /** Type of the input parameters. */
+    typedef typename Superclass::ParametersType ParametersType;
+    
+    /** Type of the Jacobian matrix. */
+    typedef typename Superclass::JacobianType JacobianType;
+    
+    /** Standard coordinate point type for this class. */
+    typedef typename Superclass::InputPointType InputPointType;
+    typedef typename Superclass::OutputPointType OutputPointType;
+    
+    /** Dimension of the domain space. */
+    itkStaticConstMacro(InputSpaceDimension,
+      unsigned int,
+      ForwardTransform::OutputSpaceDimension);
+    itkStaticConstMacro(OutputSpaceDimension,
+      unsigned int,
+      ForwardTransform::InputSpaceDimension);
+    
+    /** Set the forward transform pointer. */
+    void SetForwardTransform(const ForwardTransform * forward)
+    { forward_ = forward; }
+    
+    /**  Method to transform a point. */
+    virtual OutputPointType TransformPoint(const InputPointType & y) const
+    {
+      assert(forward_ != NULL);
+      return forward_->BackTransformPoint(y);
+    }
+    
+    virtual const JacobianType & GetJacobian(const InputPointType &) const
+    {
+      itkExceptionMacro(<< "GetJacobian is not implemented "
+      "for InverseTransform");
+      return this->m_Jacobian;
+    };
+    
+    virtual unsigned int GetNumberOfParameters() const 
+    { return 0; }
+    
+    virtual InverseTransformPointer GetInverse() const
+    { return const_cast<ForwardTransform *>(forward_); }
+    
+  protected:
+    InverseTransform(): Superclass(0, 0) {}
+    
+  private:
+    // disable default copy constructor and assignment operator:
+    InverseTransform(const Self & other);
+    const Self & operator = (const Self & t);
+    
+    // the transform whose inverse we are trying to evaluate:
+    const ForwardTransform * forward_;
+  };
+  
+} // namespace itk
+
+#endif // __itkInverseTransform_h
