7a912dee2d89d74b6c6c51331d06253815bc01fd
Ayla Khan <ayla@sci.utah.edu>
2016-04-28 16:32:27 -0600

    Add dialog to get slice range input for punching through volume.

--- src/Interface/Application/SliceRangeDialog.cc
+++ src/Interface/Application/SliceRangeDialog.cc
@@ -1,22 +1,22 @@
 /*
  For more information, please see: http://software.sci.utah.edu
- 
+
  The MIT License
- 
+
  Copyright (c) 2015 Scientific Computing and Imaging Institute,
  University of Utah.
- 
- 
+
+
  Permission is hereby granted, free of charge, to any person obtaining a
  copy of this software and associated documentation files (the "Software"),
  to deal in the Software without restriction, including without limitation
  the rights to use, copy, modify, merge, publish, distribute, sublicense,
  and/or sell copies of the Software, and to permit persons to whom the
  Software is furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included
  in all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -24,41 +24,39 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  DEALINGS IN THE SOFTWARE.
-*/
+ */
 
 #include <QtUtils/Bridge/QtBridge.h>
 
-#include <Interface/Application/LayerResamplerDialog.h>
+#include <Interface/Application/SliceRangeDialog.h>
 
-#include "ui_LayerResamplerDialog.h"
+#include "ui_SliceRangeDialog.h"
 
 namespace Seg3D
 {
 
-class LayerResamplerDialogPrivate
+class SliceRangeDialogPrivate
 {
 public:
-  Ui::LayerResamplerDialog ui_;
+  Ui::SliceRangeDialog ui_;
 };
 
-LayerResamplerDialog::LayerResamplerDialog( 
-  const LayerResamplerHandle& layer_resampler, 
-  QWidget* parent ) :
-  QDialog( parent ),
-  private_( new LayerResamplerDialogPrivate )
+SliceRangeDialog::SliceRangeDialog( QWidget* parent ) :
+QDialog( parent ),
+private_( new SliceRangeDialogPrivate )
 {
   this->private_->ui_.setupUi( this );
 
-  QtUtils::QtBridge::Connect( this->private_->ui_.padding_combobox_, layer_resampler->padding_value_state_ );
-  QtUtils::QtBridge::Connect( this->private_->ui_.kernel_combobox_, layer_resampler->kernel_state_ );
-  QtUtils::QtBridge::Connect( this->private_->ui_.sigma_spinbox_, layer_resampler->gauss_sigma_state_ );
-  QtUtils::QtBridge::Connect( this->private_->ui_.cutoff_spinbox_, layer_resampler->gauss_cutoff_state_ );
-  QtUtils::QtBridge::Connect( this->private_->ui_.spline_order_, layer_resampler->spline_order_state_ );
-
-  this->private_->ui_.spline_order_->set_description( "Spline Order" );
-
-  QtUtils::QtBridge::Show( this->private_->ui_.param_widget_, layer_resampler->has_gaussian_params_state_ );
-  QtUtils::QtBridge::Show( this->private_->ui_.bspline_widget_, layer_resampler->has_bspline_params_state_ );
+//  QtUtils::QtBridge::Connect( this->private_->ui_.padding_combobox_, layer_resampler->padding_value_state_ );
+//  QtUtils::QtBridge::Connect( this->private_->ui_.kernel_combobox_, layer_resampler->kernel_state_ );
+//  QtUtils::QtBridge::Connect( this->private_->ui_.sigma_spinbox_, layer_resampler->gauss_sigma_state_ );
+//  QtUtils::QtBridge::Connect( this->private_->ui_.cutoff_spinbox_, layer_resampler->gauss_cutoff_state_ );
+//  QtUtils::QtBridge::Connect( this->private_->ui_.spline_order_, layer_resampler->spline_order_state_ );
+//
+//  this->private_->ui_.spline_order_->set_description( "Spline Order" );
+//
+//  QtUtils::QtBridge::Show( this->private_->ui_.param_widget_, layer_resampler->has_gaussian_params_state_ );
+//  QtUtils::QtBridge::Show( this->private_->ui_.bspline_widget_, layer_resampler->has_bspline_params_state_ );
 }
 
 } // end namespace Seg3D
