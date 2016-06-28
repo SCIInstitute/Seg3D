c5d884cc49746da3b8976489652bdf940ea0af76
braxton <braxtongjohnston@gmail.com>
2016-06-27 20:31:13 -0600

    Utilize built in debug methods.  Need to make threshold and level ui scale horizontally.  Not working on my build.

--- src/Application/Filters/Actions/ActionWatershedFilter.cc
+++ src/Application/Filters/Actions/ActionWatershedFilter.cc
@@ -40,7 +40,7 @@
   * 		with the _ConsoleOuput define).  See timing for low threshold/level values.  
   * 		Can be an extremely long wait; especially for 3D watersheds.
   * 	- Break segmentations into useful objects (masks, rgb, etc) it is currently
-  * 		saved as a _LabelType label (see below register action).
+  * 		saved as a WtrLabelType label (see below register action).
   * 			~ Currently uses a for loop of unsigned int's to designate N unique labels.  This
   * 				is then outputted as a single grayscale layer.  Simply implementing RGB data
   * 				types in Seg3D would significantly improve the variety of information conveyed
@@ -81,10 +81,7 @@
 #include <sstream>
 
 
-#define _ConsoleOuput true // Switch this to false to exclude console output
-#define _Timing true       // Switch this to false to exclude timing code
-
-#if (_Timing == true)
+#ifdef NDEBUG
 //TIMING STUFF
 //		Courtesy of https://github.com/arhuaco/junkcode/blob/master/emqbit-bench/bench.c
 #include <sys/time.h>
@@ -105,14 +102,16 @@ get_timestamp ()
 // NOTE: Registration needs to be done outside of any namespace
 CORE_REGISTER_ACTION( Seg3D, WatershedFilter )
 
-#define _LabelType double
-#define _PixelTypeOut _LabelType // Should be the same
-#define _ImageTypeOut itk::Image< _PixelTypeOut, 3 >
-#define _ImageTypeFilter itk::Image< unsigned long int, 3 > 
+
 
 
 namespace Seg3D
 {
+
+typedef double WtrLabelType; //WtrLabelType
+typedef WtrLabelType WtrPixelTypeOut; //WtrPixelTypeOut WtrLabelType // Should be the same
+typedef itk::Image< WtrPixelTypeOut, 3 > WtrImageTypeOut; //WtrImageTypeOut itk::Image< WtrPixelTypeOut, 3 >
+typedef itk::Image< unsigned long int, 3 > WtrImageTypeFilter; //WtrImageTypeFilter itk::Image< unsigned long int, 3 > 
 
 bool ActionWatershedFilter::validate( Core::ActionContextHandle& context )
 {
@@ -159,9 +158,16 @@ public:
 	double watershedLevel_val_;
 
 	std::vector<std::string> dst_layer_ids_;
-	std::vector<_LabelType> watershed_image_output_labels_;
-  
+	std::vector<WtrLabelType> watershed_image_output_labels_;
+
 public:
+
+	void report_debug_message( const std::string& msg )
+	{
+		std::string filter_message = this->get_filter_name() +": " + msg;
+		CORE_LOG_DEBUG( filter_message );
+	}
+	
 	// RUN:
 	// Implemtation of run of the Runnable base class, this function is called when the thread
 	// is launched.
@@ -170,6 +176,8 @@ public:
 	// a member variable of the algorithm class.
 	SCI_BEGIN_TYPED_ITK_RUN( this->src_layer_->get_data_type() )
 	{
+		this->report_debug_message( "Initializing watershedding filter..." );
+		
 		// Define the type of filter that we use.
 		typedef itk::WatershedImageFilter< TYPED_IMAGE_TYPE > filter_type;
 		
@@ -195,16 +203,11 @@ public:
 		// Run the actual ITK filter.
 		// This needs to be in a try/catch statement as certain filters throw exceptions when they
 		// are aborted. In that case we will relay a message to the status bar for information.
-#if (_ConsoleOuput == true)
-		std::cout << "\nRunning watershed filter:\n";
-		std::cout << "\tPROGRESS :: Updating watershedding filter...\n";
-#endif
-		try 
-		{ 
+		this->report_debug_message( "Updating watershedding filter..." );
+		try{ 
 			filter->Update(); 
 		} 
-		catch ( ... ) 
-		{
+		catch ( ... ){
 			if ( this->check_abort() )
 			{
 				this->report_error( "Filter was aborted." );
@@ -217,44 +220,41 @@ public:
 		// As ITK filters generate an inconsistent abort behavior, we record our own abort flag
 		// This one is set when the abort button is pressed and an abort is sent to ITK.
 		if ( this->check_abort() ) return;
-#if (_ConsoleOuput == true)
-		std::cout << "\tPROGRESS :: Casting filter to output type...\n";
-#endif
-		typedef itk::CastImageFilter< _ImageTypeFilter, _ImageTypeOut > CastImageFilter_FilterToOut;
+		
+		this->report_debug_message( "Casting filter to output type..." );
+		typedef itk::CastImageFilter< WtrImageTypeFilter, WtrImageTypeOut > CastImageFilter_FilterToOut;
 		CastImageFilter_FilterToOut::Pointer castImage_FilterToOut = CastImageFilter_FilterToOut::New();
 		castImage_FilterToOut->SetInput( filter->GetOutput() );
 		castImage_FilterToOut->Update();
-#if (_ConsoleOuput == true)
-		std::cout << "\tPROGRESS :: Reading in all labels (extreme patience required)...\n";
-#endif
-#if (_Timing == true)
+		
+		this->report_debug_message( "Reading in all labels (extreme patience required)..." );
+#ifdef NDEBUG
 		timestamp_t t0 = get_timestamp();
 #endif
 		// Relabel the output
 		// The watershedding algorithm splits everything up into labels.  So let's get em all and then output each label into it's own mask
 		// This is from Dr. Jurrus's example ITK Watershed example.  No specific author is apparent.  I have a copy of the code if needed.
-		itk::ImageRegionIterator< _ImageTypeOut > wtrImg_iter ( castImage_FilterToOut->GetOutput(), filter->GetOutput()->GetLargestPossibleRegion() );
+		itk::ImageRegionIterator< WtrImageTypeOut > wtrImg_iter ( castImage_FilterToOut->GetOutput(), filter->GetOutput()->GetLargestPossibleRegion() );
 		int iterCount = 0;
 		for( wtrImg_iter.GoToBegin(); !wtrImg_iter.IsAtEnd(); ++wtrImg_iter )
 		{
 		   iterCount += 1;
-		   _LabelType val = wtrImg_iter.Get();
+		   WtrLabelType val = wtrImg_iter.Get();
 		   if ( find( this->watershed_image_output_labels_.begin(), this->watershed_image_output_labels_.end(), val ) == this->watershed_image_output_labels_.end() ){
-			   //std::cout << "label val: " << val << std::endl;
 			   this->watershed_image_output_labels_.push_back( val );
 			}
 		}
-#if (_Timing == true)
+#ifdef NDEBUG
 		timestamp_t t1 = get_timestamp();
 		double secs = (t1 - t0) / 1000000.0L;
+		
+		std::ostringstream oss_msg;
+		oss_msg << "\t" << iterCount << " iterations for " << this->watershed_image_output_labels_.size() << " labels in " << secs << " seconds.";
+		this->report_debug_message( oss_msg.str() );
 #endif
-#if ((_ConsoleOuput == true) && (_Timing == true))
-		std::cout << "\t\t\t" << iterCount << " iterations for " << this->watershed_image_output_labels_.size() << " labels in " << secs << " seconds.\n";
-#endif
-#if (_ConsoleOuput == true)
-		std::cout << "\tPROGRESS :: Creating new labels...\n";
-#endif
-		std::vector< _LabelType > new_labels;
+		this->report_debug_message( "Creating new labels..." );
+		
+		std::vector< WtrLabelType > new_labels;
 		// Could be an issue if the size of watershed_image_ouput_labels is larger than unsigned int can handle.  Doubt it, but still...
 		for( unsigned int n=1; n<=this->watershed_image_output_labels_.size(); n++ ){
 			new_labels.push_back( n );
@@ -268,12 +268,12 @@ public:
  * --[CODE]--
  * #include <random>
  *		
- * typedef itk::RGBPixel< _LabelType > RGBPixelType;
+ * typedef itk::RGBPixel< WtrLabelType > RGBPixelType;
  * typedef itk::Image< RGBPixelType, 3 > RGBImageType;
  * 
  * 	std::vector< RGBPixelType > watershed_image_ouput_rgb_labels; // As a member of WatershedFilterAlgo class
  * 
- * 		typedef itk::CastImageFilter< _ImageTypeOut, RGBImageType > CastImageFilter_OutToRGB; // Would be more efficient to make RGB the Out
+ * 		typedef itk::CastImageFilter< WtrImageTypeOut, RGBImageType > CastImageFilter_OutToRGB; // Would be more efficient to make RGB the Out
  *		CastImageFilter_OutToRGB::Pointer castImage_OutToRGB = CastImageFilter_OutToRGB::New();
  *		castImage_OutToRGB->SetInput( castImage_FilterToOut->GetOutput() );
  *		castImage_OutToRGB->Update();
@@ -308,41 +308,29 @@ public:
  * on the castImage_OutToRGB image object in order to use the rgb labels.  Also,
  * make sure to insert the rgb image into the layer
  */ 		
-#if (_ConsoleOuput == true)
-		std::cout << "\tPROGRESS :: Shuffling new labels...\n";
-#endif
+ 
+		this->report_debug_message( "Shuffling new labels..." );
 		random_shuffle( new_labels.begin(), new_labels.end() );
 		
-#if (_ConsoleOuput == true)
-		std::cout << "\tPROGRESS :: Mapping old labels to new...\n";
-#endif
-		std::map< _LabelType, _LabelType > label_map;
+		this->report_debug_message( "Mapping old labels to new..." );
+		std::map< WtrLabelType, WtrLabelType > label_map;
 		for( unsigned int i=0; i<this->watershed_image_output_labels_.size(); i++ ){
 			label_map[ this->watershed_image_output_labels_[i] ] = new_labels[i];
 		}
-
-#if (_ConsoleOuput == true)
-		std::cout << "\tPROGRESS :: Setting new labels...\n";
-#endif
+		
+		this->report_debug_message( "Setting new labels..." );
 		for( wtrImg_iter.GoToBegin(); !wtrImg_iter.IsAtEnd(); ++wtrImg_iter )
 		{
 			wtrImg_iter.Set( label_map[ wtrImg_iter.Get() ] );
 		}
-
-#if (_ConsoleOuput == true)
-		std::cout << "\tPROGRESS :: Linking new labels...\n";
-#endif
+		
+		this->report_debug_message( "Linking new labels..." );
 		this->watershed_image_output_labels_ = new_labels;
 		
-#if (_ConsoleOuput == true)
-		std::cout << "\tPROGRESS :: Inserting output into layer...\n";
-#endif
+		this->report_debug_message( "Inserting output into layer..." );
 		this->insert_itk_image_into_layer( this->dst_layer_, castImage_FilterToOut->GetOutput() );
 		
-#if (_ConsoleOuput == true)
-		std::cout << "\tPROGRESS :: Finished.\n\n";
-#endif
-
+		this->report_debug_message( "Finished.\n" );
 	}
 	SCI_END_TYPED_ITK_RUN()
   
@@ -359,8 +347,8 @@ public:
 	virtual std::string get_layer_prefix() const
 	{
 		std::ostringstream oss;
-		oss << "Watershed  ::  thresh_" << Core::ExportToString(this->watershedThreshold_val_) << "  --  lvl_"
-		    << Core::ExportToString(this->watershedLevel_val_) << "  ::  ";
+		oss << "Watershed_threshold_" << Core::ExportToString(this->watershedThreshold_val_) << "_level_"
+		    << Core::ExportToString(this->watershedLevel_val_) << "_";
 		return oss.str();
 	}
 };
