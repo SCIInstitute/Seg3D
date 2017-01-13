a8219a712eb9fa8607d16e77a3c1ec71c36066fb
Ayla Khan <ayla@sci.utah.edu>
2017-01-12 23:57:26 -0700

    Data download mod (#181)
    * Add option to not download data.
    
    * Use option to not enable data. Try Seg3D-only build.
    
    * Experiment with make targets.
    
    * Try parallel build.
    
    * Try trusty qt5 packages.
    
    * More trusty qt5 packages.
    
    * Try another PPA. May have to revert from container build.
    
    * Disable container build.
    
    * Add Qt5 SVG package. Experiment with build matrix.
    
    * Re-arrange build.
    
    * Fix syntax error.
    
    * Try speeding up the build by turning off compiler warnings in thirdparty libs.
    
    * Add travis build configuration.
    
    * Build is stalling?
    
    * Try headless build to avoid timeout.
    
    * Reposition Travis-CI configuration.
    
    * Add GL/Mesa packages.
    
    * Disable caching externals. Use quiet apt-get flags.
    
    * Add package for GLU library.
    
    * Try suppressing most text output again.
    
    * Experiment with travis_wait (tricky).
    
    * Just try 8 proc.
    
    * Cleanup libraries.
    
    * The Application_Socket library depends on python, so don't include if not compiling Python support.
    
    * The Application_Socket library depends on python, so don't include if not compiling Python support. Add directive for CoreGraphics, since rendering libraries are still compiled.
    
    * Turn off Python in Travis builds.
    
    * Turning off ITK build default modules setting appears to work for Travis-CI build settings.
    
    * See if ninja is whitelisted for trusty.
    
    * Try ninja build.
    
    * Fix typo.
    
    * Add ninja the old way.
    
    * Try travis_wait.
    
    * Increase time.
    
    * Add build status.
    
    * Try re-enabling GUI build.
    
    * Update README.md
    
    Fix build status.
    
    * Re-enable qt package install.
    
    * Forgot Qt path.
    
    * Try max time limit.
    
    * GUI build still timing out.
    
    * Fix syntax error.

--- Superbuild/SQLiteExternal.cmake
+++ Superbuild/SQLiteExternal.cmake
@@ -1,21 +1,21 @@
 #  For more information, please see: http://software.sci.utah.edu
-# 
+#
 #  The MIT License
-# 
+#
 #  Copyright (c) 2016 Scientific Computing and Imaging Institute,
 #  University of Utah.
-# 
-#  
+#
+#
 #  Permission is hereby granted, free of charge, to any person obtaining a
 #  copy of this software and associated documentation files (the "Software"),
 #  to deal in the Software without restriction, including without limitation
 #  the rights to use, copy, modify, merge, publish, distribute, sublicense,
 #  and/or sell copies of the Software, and to permit persons to whom the
 #  Software is furnished to do so, subject to the following conditions:
-# 
+#
 #  The above copyright notice and this permission notice shall be included
-#  in all copies or substantial portions of the Software. 
-# 
+#  in all copies or substantial portions of the Software.
+#
 #  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 #  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 #  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -26,6 +26,11 @@
 
 SET_PROPERTY(DIRECTORY PROPERTY "EP_BASE" ${ep_base})
 SET(sqlite_GIT_TAG "origin/master")
+
+IF(TRAVIS_BUILD)
+  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -w")
+  SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -w")
+ENDIF()
 
 # If CMake ever allows overriding the checkout command or adding flags,
 # git checkout -q will silence message about detached head (harmless).
@@ -39,6 +44,8 @@ ExternalProject_Add(SQLite_external
     -DCMAKE_VERBOSE_MAKEFILE:BOOL=${CMAKE_VERBOSE_MAKEFILE}
     -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
     -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
+    -DCMAKE_CXX_FLAGS:STATIC=${CMAKE_CXX_FLAGS}
+    -DCMAKE_C_FLAGS:STATIC=${CMAKE_C_FLAGS}
 )
 
 ExternalProject_Get_Property(SQLite_external BINARY_DIR)
