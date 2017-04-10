bde33453c5932244b9bd99e061a0873bcf2b99f2
Ayla Khan <ayla@sci.utah.edu>
2017-04-10 17:18:02 -0600

    Add basic connect/disconnect test.

--- src/Application/Socket/Tests/ActionSocketTests.cc
+++ src/Application/Socket/Tests/ActionSocketTests.cc
@@ -32,9 +32,11 @@
 #include <iostream>
 #include <clocale>
 #include <cstdlib>
+#include <string>
 
 #include <boost/filesystem.hpp>
-#include <boost/preprocessor.hpp>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
 
 #include <Python.h>
 #include <Core/Python/PythonInterpreter.h>
@@ -45,6 +47,8 @@ using namespace Core;
 using namespace Seg3D;
 using namespace ::testing;
 using namespace boost::filesystem;
+using namespace boost::asio::ip;
+
 
 class ActionSocketTests : public Test
 {
@@ -63,18 +67,82 @@ protected:
 
 TEST_F(ActionSocketTests, StartSocket)
 {
+  const int PORT = 9400;
+  const std::string PORT_STRING("9400");
+
   Core::PythonInterpreter::module_list_type python_modules;
   Core::PythonInterpreter::Instance()->initialize( python_wstr, python_modules );
   Core::PythonInterpreter::Instance()->run_string( python_import_module );
   Core::PythonInterpreter::Instance()->run_string( python_import_from_module );
 
-  const int PORT = 9400;
-  const std::string PORT_FILE("port");
   ActionSocket::Instance()->start( PORT );
+}
+
+TEST_F(ActionSocketTests, ConnectToSocket)
+{
+  const int PORT = 9401;
+  const std::string PORT_STRING("9401");
+  const std::string PORT_FILE("port");
+
+  Core::PythonInterpreter::module_list_type python_modules;
+  Core::PythonInterpreter::Instance()->initialize( python_wstr, python_modules );
+  Core::PythonInterpreter::Instance()->run_string( python_import_module );
+  Core::PythonInterpreter::Instance()->run_string( python_import_from_module );
+
+  ActionSocket::Instance()->start( PORT );
+
+  boost::asio::io_service io_service;
+  tcp::resolver resolver(io_service);
+  tcp::resolver::query query("localhost", PORT_STRING);
+  tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+  tcp::socket socket(io_service);
+  boost::asio::connect(socket, endpoint_iterator);
+
+  // do basic connect and disconnect from server
+  for (;;)
+  {
+    boost::asio::streambuf buffer;
+    boost::system::error_code error, ignored_error;
+
+    size_t len = boost::asio::read_until(socket, buffer, "\r\n", error);
+    ASSERT_GT(len, 0);
+
+    if ( (! error ) || ( error == boost::asio::error::eof ) )
+    {
+      std::istream is(&buffer);
+      std::string message;
+      std::getline(is, message);
+      ASSERT_STREQ(message.c_str(), "Welcome to Seg3D\r");
+    }
+    else
+    {
+      throw boost::system::system_error(error); // Some other error.
+    }
+
+    // disconnect (cleanly)
+    boost::asio::write( socket, boost::asio::buffer( "exit\r\n" ), ignored_error);
+    len = boost::asio::read_until(socket, buffer, "\r\n", error);
+    ASSERT_GT(len, 0);
+
+    if ( (! error ) || ( error == boost::asio::error::eof ) )
+    {
+      std::istream is(&buffer);
+      std::string message;
+      std::getline(is, message);
+      ASSERT_STREQ(message.c_str(), "Goodbye!\r");
+
+      break;
+    }
+    else
+    {
+      throw boost::system::system_error(error); // Some other error.
+    }
+  }
 
   bool portFileExists = exists( PORT_FILE );
   ASSERT_TRUE(portFileExists);
-
+  
   std::ifstream inputfile;
   inputfile.open( PORT_FILE.c_str() );
   int port = 0;
