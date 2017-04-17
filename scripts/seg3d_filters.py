ecd9ce4fc1a994c0a6354d5147f711d428a968d2
Ayla Khan <ayla@sci.utah.edu>
2017-04-17 13:08:21 -0600

    Save batch processing scripts.

--- scripts/seg3d_filters.py
+++ scripts/seg3d_filters.py
@@ -6,19 +6,39 @@ import os, sys, types, threading
 
 import seg3d2
 
+# since this is a batch script, truth_region, etc. need to be set beforehand
+# i.e. set as command via socket
+if not truth_region:
+  raise ValueError
+if not truth_cropped_region:
+  raise ValueError
+if not im_gray:
+  raise ValueError
+if not im_cropped_gray:
+  raise ValueError
+if not im_cropped_chm:
+  raise ValueError
+if not im_gray_test:
+  raise ValueError
+if not im_cropped_gray_test:
+  raise ValueError
+if not im_cropped_chm_test:
+  raise ValueError
+
 class MyThread(threading.Thread):
-  def __init__(self, layerID, timeout=2.0):
+  def __init__(self, layerID, timeout=5.0):
     self.layerID = layerID
     self.TIMEOUT = timeout
     self.condition = threading.Condition()
     threading.Thread.__init__(self)
   def run(self):
+    availableState = "available"
     stateIDData = self.layerID + "::data"
     layerStatus = get(stateid=stateIDData)
     #print(self.layerID, " ", layerStatus)
     with self.condition:
-      self.condition.wait_for(lambda: "available" == get(stateid=stateIDData), timeout=self.TIMEOUT)
-    #print("Layer {} done processing".format(self.layerID))
+      while get(stateid=stateIDData) != availableState:
+        self.condition.wait_for(lambda: get(stateid=stateIDData) == availableState, timeout=self.TIMEOUT)
 
 def update_filepath(filename, strings):
   newfilename = filename
@@ -26,50 +46,52 @@ def update_filepath(filename, strings):
     newfilename = newfilename.replace(key, value)
   return newfilename
 
-def wait_on_layer(layer):
-  thread = MyThread(layer, 1.0)
+def wait_on_layer(layer, timeout=2.0):
+  thread = MyThread(layer, timeout)
   thread.start()
   thread.join()
 
-# since this is a batch script, truth_region and im_gray need to be set beforehand
-# i.e. set as command via socket
-if not truth_region:
-  raise ValueError
-
-if not im_gray:
-  raise ValueError
-
 truth_region_files = [os.path.join(truth_region, f) for f in os.listdir(truth_region) if os.path.isfile(os.path.join(truth_region, f)) and f.lower().endswith('.png')]
 im_gray_files = [os.path.join(im_gray, f) for f in os.listdir(im_gray) if os.path.isfile(os.path.join(im_gray, f)) and f.lower().endswith('.mha')]
+im_gray_files_test = [os.path.join(im_gray_test, f) for f in os.listdir(im_gray_test) if os.path.isfile(os.path.join(im_gray_test, f)) and f.lower().endswith('.mha')]
+
+gray_files=im_gray_files+im_gray_files_test
 
 # ground truth segmentations:
 # import -> crop -> export
 for f in truth_region_files:
-  layers = importlayer(filename="{}".format(f),importer='[ITK Importer]',mode='mask')
-  print(layers[0])
+  layers = importlayer(filename="{}".format(f),importer='[ITK Importer]',mode='single_mask')
   wait_on_layer(layers[0])
-  layers = crop(layerids="{}".format(layers[0]),origin='[-0.0735295,-0.0735295,-0.5]',size='[150.515,129.927,1]',replace='false')
-  print(layers[0])
+  layers = crop(layerids="{}".format(layers[0]),origin='[-0.5,-0.5,-0.5]',size='[1024,883.5,1]',replace='true')
   wait_on_layer(layers[0])
-  export_file = update_filepath(f, { truth_region: truth_cropped_region })
-  print(export_file)
-  retval = exportlayer(layer="{}".format(layers[0]),file_path='{}'.format(export_file),extension='.mha',exporter='[ITK Mask Exporter]');
+  retval = exportsegmentation(layers="{}".format(layers[0]),file_path='{}'.format(truth_cropped_region),mode='single_mask',extension='.png')
+  if not retval:
+    print("exportsegmentation failed")
 
 # input data images:
 # import -> crop -> gradient anisotropic diffusion -> gradient magnitude (boundaries) -> export
-for f in im_gray_files:
+for f in gray_files:
+  # crop and export grayscale raw input images
   layers = importlayer(filename="{}".format(f),importer='[ITK Importer]',mode='data')
-  print(layers[0])
   wait_on_layer(layers[0])
-  layers = crop(layerids="{}".format(layers[0]),origin='[-0.0735295,-0.0735295,-0.5]',size='[150.515,129.927,1]',replace='false')
-  print(layers[0])
+  layers = transform(layerids="{}".format(layers[0]),origin='[0,0,0]',spacing='[1,1,1]',replace='true')
   wait_on_layer(layers[0])
-  layers = gradientanisotropicdiffusionfilter(layerid=="{}".format(layers[0])preserve_data_format='true',replace='false',iterations='20',sensitivity='0.25');
-  print(layers[0])
-  wait_on_layer(layers[0])
-  layers = gradientmagnitudefilter(layerid="{}".format(layers[0]),replace='false',preserve_data_format='true');
-  print(layers[0])
-  wait_on_layer(layers[0])
-  export_file = update_filepath(f, { im_gray: im_cropped_gray })
-  print(export_file)
-  retval = exportlayer(layer="{}".format(layers[0]),file_path='{}'.format(export_file),extension='.mha',exporter='[ITK Data Exporter]');
+  layers = crop(layerids="{}".format(layers[0]),origin='[-0.5,-0.5,-0.5]',size='[1024,883.5,1]',replace='true')
+  layer = layers[0]
+  wait_on_layer(layer)
+  export_file = update_filepath(f, { im_gray_test: im_cropped_gray_test, im_gray: im_cropped_gray })
+  retval = exportlayer(layer="{}".format(layer),file_path='{}'.format(export_file),extension='.mha',exporter='[ITK Data Exporter]')
+  if not retval:
+    print("exportlayer failed")
+
+  # get boundaries
+  #layer = gradientanisotropicdiffusionfilter(layerid="{}".format(layer),preserve_data_format='true',replace='true',iterations='20',sensitivity='0.25')
+  #wait_on_layer(layer, 5.0)
+  layer = gradientanisotropicdiffusionfilter(layerid="{}".format(layer),preserve_data_format='true',replace='true',iterations='2',sensitivity='0.25')
+  wait_on_layer(layer)
+  layer = gradientmagnitudefilter(layerid="{}".format(layer),replace='true',preserve_data_format='true')
+  wait_on_layer(layer)
+  export_file = update_filepath(f, { im_gray_test: im_cropped_chm_test, im_gray: im_cropped_chm })
+  retval = exportlayer(layer="{}".format(layer),file_path='{}'.format(export_file),extension='.mha')
+  if not retval:
+    print("exportlayer failed")
