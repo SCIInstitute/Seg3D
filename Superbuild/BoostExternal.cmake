4fc762e2c9d773be157070497c91484e2096418a
Dan White <dwhite@sci.utah.edu>
2019-01-18 14:06:16 -0700

    Builds with Qt 5.12, python on, testing off

--- Superbuild/BoostExternal.cmake
+++ Superbuild/BoostExternal.cmake
@@ -66,7 +66,7 @@ IF(WIN32)
   SET(FORCE_64BIT_BUILD ON)
 ENDIF()
 
-SET(boost_GIT_TAG "origin/master")
+SET(boost_GIT_TAG "origin/v1.67.0")
 SET(boost_GIT_URL "https://github.com/CIBC-Internal/boost.git")
 
 IF(TRAVIS_BUILD)
