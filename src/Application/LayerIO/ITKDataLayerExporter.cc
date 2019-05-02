64b7c3147262122f12fa56bb910a34ee13927364
allywarner <avw0731@gmail.com>
2019-05-02 12:21:58 -0600

    Signed short to unsigned short - WIP
    Scaling doesn't work.

--- src/Application/LayerIO/ITKDataLayerExporter.cc
+++ src/Application/LayerIO/ITKDataLayerExporter.cc
@@ -38,6 +38,7 @@
 #include <itkMetaDataObject.h>
 #include <itkRescaleIntensityImageFilter.h>
 #include <itkCastImageFilter.h>
+#include <itkMinimumMaximumImageCalculator.h>
 
 // GDCM includes
 #include <gdcmImageHelper.h>
@@ -75,8 +76,13 @@ cast_image( typename Core::ITKImageDataT< InputPixelType >::Handle image_data )
   typedef itk::RescaleIntensityImageFilter< InputImageType, InputImageType > RescaleType;
   typename RescaleType::Pointer rescale = RescaleType::New();
 
+  //static_assert((std::is_unsigned<InputPixelType>::value && std::is_unsigned<OutputPixelType>::value) 
+    //|| (std::is_signed<InputPixelType>::value && std::is_signed<OutputPixelType>::value), "Signed/unsigned mismatch");
+
   // assumes casting to type with smaller range...
   rescale->SetInput( itk_image );
+  rescale->Update();
+
   rescale->SetOutputMinimum( itk::NumericTraits< OutputPixelType >::min() );
   // TODO: problem for PNG?
   rescale->SetOutputMaximum( itk::NumericTraits< OutputPixelType >::max() );
@@ -85,6 +91,38 @@ cast_image( typename Core::ITKImageDataT< InputPixelType >::Handle image_data )
   typename CastFilterType::Pointer castFilter = CastFilterType::New();
   castFilter->SetInput( rescale->GetOutput() );
   castFilter->Update();
+
+  return castFilter->GetOutput();
+}
+
+template< class InputPixelType, class OutputPixelType >
+typename itk::Image< OutputPixelType, DIM_3D >::Pointer
+direct_cast_image(typename Core::ITKImageDataT< InputPixelType >::Handle image_data)
+{
+  typedef itk::Image< InputPixelType, DIM_3D > InputImageType;
+  typedef itk::Image< OutputPixelType, DIM_3D > OutputImageType;
+  typedef typename Core::ITKImageDataT< OutputPixelType > OutputType;
+
+  typename InputImageType::Pointer itk_image = image_data->get_image();
+
+  typedef itk::MinimumMaximumImageCalculator<InputImageType> MinimumMaximumImageCalculatorType;
+  typename MinimumMaximumImageCalculatorType::Pointer MinMaxCalc = MinimumMaximumImageCalculatorType::New();
+  MinMaxCalc->SetImage(itk_image);
+  MinMaxCalc->Compute();
+  auto inputMain = MinMaxCalc->GetMinimum();
+  auto inputMax = MinMaxCalc->GetMaximum();
+
+  typedef itk::CastImageFilter< InputImageType, OutputImageType > CastFilterType;
+  typename CastFilterType::Pointer castFilter = CastFilterType::New();
+  castFilter->SetInput(itk_image);
+  castFilter->Update();
+
+  typedef itk::MinimumMaximumImageCalculator<OutputImageType> MinimumMaximumImageCalculatorCastType;
+  auto MinMaxCalcCast = MinimumMaximumImageCalculatorCastType::New();
+  MinMaxCalcCast->SetImage(castFilter->GetOutput());
+  MinMaxCalcCast->Compute();
+  auto inputMain_1 = MinMaxCalcCast->GetMinimum();
+  auto inputMax_1 = MinMaxCalcCast->GetMaximum();
 
   return castFilter->GetOutput();
 }
@@ -459,11 +497,12 @@ bool ITKDataLayerExporter::export_layer_internal( const std::string& file_path,
                    temp_handle->get_grid_transform() ) );
 
   bool success = false;
-  if ( this->extension_ == ".dcm" )
+  if ( this->extension_ == ".dcm" || this->extension_ == ".dicom" || this->extension_ == ".ima")
   {
     if ( this->pixel_type_ == Core::DataType::FLOAT_E || this->pixel_type_ == Core::DataType::DOUBLE_E ||
          this->pixel_type_ == Core::DataType::ULONGLONG_E || this->pixel_type_ == Core::DataType::LONGLONG_E )
     {
+      //investigate unsigned long long to int conversion
       typename itk::Image< int, DIM_3D >::Pointer new_image_data = cast_image< InputPixelType, int >( image_data );
       success =  export_dicom_series< int >( file_path, name, new_image_data, temp_handle, this->extension_ );
     }
@@ -494,7 +533,7 @@ bool ITKDataLayerExporter::export_layer_internal( const std::string& file_path,
     if (! ( this->pixel_type_ == Core::DataType::CHAR_E || this->pixel_type_ == Core::DataType::UCHAR_E ||
             this->pixel_type_ == Core::DataType::USHORT_E ) )
     {
-      typename itk::Image< unsigned short, DIM_3D >::Pointer new_image_data = cast_image< InputPixelType, unsigned short >( image_data );
+      typename itk::Image< unsigned short, DIM_3D >::Pointer new_image_data = cast_image< InputPixelType, unsigned short >(image_data);
       success = export_image_series< unsigned short >( file_path, name, new_image_data, this->extension_ );
     }
     else
