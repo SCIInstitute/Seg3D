3edfe06eec3eae84ad867dc0c68b252d831a4509
Ally Warner <avw0731@gmail.com>
2020-10-30 13:05:36 -0600

    Update ITKExternal.cmake

--- Superbuild/ITKExternal.cmake
+++ Superbuild/ITKExternal.cmake
@@ -50,7 +50,9 @@ SET(itk_CACHE_ARGS
   "-DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}"
   "-DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>"
   "-DITK_BUILD_DEFAULT_MODULES:BOOL=OFF"
-  "-DITKV3_COMPATIBILITY:BOOL=OFF"
+  "-DITKV4_COMPATIBILITY:BOOL=ON"
+  "-DITK_LEGACY_REMOVE:BOOL=OFF"
+  "-DITK_FUTURE_LEGACY_REMOVE:BOOL=OFF"
   "-DModule_ITKRegistrationCommon:BOOL=ON"
   "-DModule_ITKSmoothing:BOOL=ON"
   "-DModule_ITKAnisotropicSmoothing:BOOL=ON"
@@ -68,12 +70,13 @@ SET(itk_CACHE_ARGS
   "-DModule_ITKIOGIPL:BOOL=ON"
   "-DModule_IITKIOMeta:BOOL=ON"
   "-DModule_ITKIONRRD:BOOL=ON"
-  "-DModule_ITKDeprecated:BOOL=ON"
+  "-DModule_ITKDeprecated:BOOL=OFF"
   "-DCMAKE_CXX_FLAGS:STATIC=${CMAKE_CXX_FLAGS}"
   "-DCMAKE_CXX_FLAGS_DEBUG:STATIC=${CMAKE_CXX_FLAGS_DEBUG}"
   "-DCMAKE_C_FLAGS:STATIC=${CMAKE_C_FLAGS}"
   "-DCMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD:STATIC=${CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD}"
   "-DCMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY:STATIC=${CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY}"
+  "-DHDF5_BUILD_HL_LIB:BOOL=ON"
 )
 
 IF(BUILD_MOSAIC_TOOLS)
@@ -86,12 +89,12 @@ IF(BUILD_MOSAIC_TOOLS)
   )
 ENDIF()
 
-SET(itk_GIT_TAG "origin/itk5.1.0")
+SET(itk_GIT_TAG "enableH5HL")
 
 # If CMake ever allows overriding the checkout command or adding flags,
 # git checkout -q will silence message about detached head (harmless).
 ExternalProject_Add(ITK_external
-  GIT_REPOSITORY "https://github.com/CIBC-Internal/itk.git"
+  GIT_REPOSITORY "https://github.com/InsightSoftwareConsortium/ITK.git"
   GIT_TAG ${itk_GIT_TAG}
   UPDATE_COMMAND ""
   PATCH_COMMAND ""
@@ -101,6 +104,6 @@ ExternalProject_Add(ITK_external
 
 # hardcoded, since we need this before ITK's configure step
 ExternalProject_Get_Property(ITK_external INSTALL_DIR)
-SET(ITK_DIR "${INSTALL_DIR}/lib/cmake/ITK-5.1" CACHE PATH "")
+SET(ITK_DIR "${INSTALL_DIR}/lib/cmake/ITK-5.2" CACHE PATH "")
 
 MESSAGE(STATUS "ITK_DIR=${ITK_DIR}")
